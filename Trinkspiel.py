33da3fcc4854676ad2521a6e0568c6ee46ac16de
Schildizz <74683037+Schildizz@users.noreply.github.com>
2020-11-19 10:43:40 +0100

    Add files via upload

---
+++ Trinkspiel.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import os
+
+def Aufgabe():
+    return os.system("")
+
+def Wahrheit():
+    return os.system("")
+
+
+master= Tk()
+master.geometry("150x150")
+
+master.configure(bg='light blue')
+
+Button(master, text="Aufgabe", command=Aufgabe).place(x=20, y=20)
+Button(master, text="Wahrheit", command=Wahrheit).place(x=25, y=50)
+
+
+mainloop()
